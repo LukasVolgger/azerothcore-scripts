60efb7e45ba7192080b0664f09e7266f42813ab2
Lukas Volgger <lukas.volgger@live.at>
2023-07-30 21:49:10 +0200

    refactoring

--- sql/admin/01_create_admin_accounts.sql
+++ sql/admin/01_create_admin_accounts.sql
@@ -5,19 +5,19 @@
 -- dont forget to set a password in worldserver console:
 -- account set password 'accountname' 'password' 'password'
 
--- create accounts
+-- account
 DELETE FROM `account` WHERE `id` IN (1, 2, 3);
 INSERT INTO `account` (`id`, `username`, `salt`, `verifier`, `session_key`, `totp_secret`, `email`, `reg_mail`, `joindate`, `last_ip`, `last_attempt_ip`, `failed_logins`, `locked`, `lock_country`, `last_login`, `online`, `expansion`, `mutetime`, `mutereason`, `muteby`, `locale`, `os`, `recruiter`, `totaltime`) VALUES (1, 'ADMIN', 'salt', 'verifier', NULL, NULL, '', '', CURRENT_TIMESTAMP(), '127.0.0.1', '127.0.0.1', 0, 0, '00', NULL, 0, 2, 0, '', '', 0, '', 0, 0);
 INSERT INTO `account` (`id`, `username`, `salt`, `verifier`, `session_key`, `totp_secret`, `email`, `reg_mail`, `joindate`, `last_ip`, `last_attempt_ip`, `failed_logins`, `locked`, `lock_country`, `last_login`, `online`, `expansion`, `mutetime`, `mutereason`, `muteby`, `locale`, `os`, `recruiter`, `totaltime`) VALUES (2, 'ALLIANCEADMIN', 'salt', 'verifier', NULL, NULL, '', '', CURRENT_TIMESTAMP(), '127.0.0.1', '127.0.0.1', 0, 0, '00', NULL, 0, 2, 0, '', '', 0, '', 0, 0);
 INSERT INTO `account` (`id`, `username`, `salt`, `verifier`, `session_key`, `totp_secret`, `email`, `reg_mail`, `joindate`, `last_ip`, `last_attempt_ip`, `failed_logins`, `locked`, `lock_country`, `last_login`, `online`, `expansion`, `mutetime`, `mutereason`, `muteby`, `locale`, `os`, `recruiter`, `totaltime`) VALUES (3, 'HORDEADMIN', 'salt', 'verifier', NULL, NULL, '', '', CURRENT_TIMESTAMP(), '127.0.0.1', '127.0.0.1', 0, 0, '00', NULL, 0, 2, 0, '', '', 0, '', 0, 0);
 
--- set accounts access to gmlevel 3
+-- account_access
 DELETE FROM `account_access` WHERE `id` IN (1, 2, 3);
 INSERT INTO `account_access` (`id`, `gmlevel`, `RealmID`, `comment`) VALUES (1, 3, 1, '');
 INSERT INTO `account_access` (`id`, `gmlevel`, `RealmID`, `comment`) VALUES (2, 3, 1, '');
 INSERT INTO `account_access` (`id`, `gmlevel`, `RealmID`, `comment`) VALUES (3, 3, 1, '');
 
--- misc
+-- realmcharacters
 DELETE FROM `realmcharacters` WHERE `acctid` IN (1, 2, 3);
 INSERT INTO `realmcharacters` (`realmid`, `acctid`, `numchars`) VALUES (1, 1, 0);
 INSERT INTO `realmcharacters` (`realmid`, `acctid`, `numchars`) VALUES (1, 2, 0);
