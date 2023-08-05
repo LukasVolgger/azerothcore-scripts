79df25397d087e12d74f64041ed403aeb2c0a4d6
Lukas Volgger <lukas.volgger@live.at>
2023-08-05 20:43:44 +0200

    feat/paladin: add class mounts script

---
+++ sql/characters/classes/paladin/2_get_paladin_mounts.sql
@@ -0,0 +1,13 @@
+SET @CHARACTER = 1;
+
+-- Warhorse
+REPLACE
+INTO `character_spell` (`guid`, `spell`, `specMask`) VALUES (@CHARACTER, 13819, 255);
+
+-- Charger
+REPLACE
+INTO `character_spell` (`guid`, `spell`, `specMask`) VALUES (@CHARACTER, 23214, 255);
+
+-- Argent Charger
+REPLACE
+INTO `character_spell` (`guid`, `spell`, `specMask`) VALUES (@CHARACTER, 66906, 255);
