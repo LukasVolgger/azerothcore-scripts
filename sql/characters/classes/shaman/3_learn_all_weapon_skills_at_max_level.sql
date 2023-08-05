6446387eb9e8c8292212cb9ac477bf28851396c9
Lukas Volgger <lukas.volgger@live.at>
2023-08-06 00:16:42 +0200

    feat/shaman: add docs and config to scripts

--- sql/characters/classes/shaman/3_learn_all_weapon_skills_at_max_level.sql
+++ sql/characters/classes/shaman/3_learn_all_weapon_skills_at_max_level.sql
@@ -1,15 +1,23 @@
--- Sets all possible weapon skills (also the learnable ones) to the highest level
--- 44 = Axes
--- 54 = Maces
--- 95 = Defense
--- 136 = Staves
--- 160 = Two-Handed Maces
--- 162 = Unarmed
--- 172 = Two-Handed Axes
--- 173 = Daggers
+/***************************************************************************************************
+Faction:            Both
+DB:                 characters
+DB Version:         2023-08-05
+Author:             Lukas Volgger
+****************************************************************************************************
+DESCRIPTION
+----------------------------------------------------------------------------------------------------
+- Sets all possible weapon skills (also the learnable ones) to the highest level
+- 44 = Axes, 54 = Maces, 95 = Defense, 136 = Staves, 160 = Two-Handed Maces, 162 = Unarmed,
+  172 = Two-Handed Axes, 173 = Daggers
+***************************************************************************************************/
 
-SET
-@CHARACTER = 1;
+/***************************************************************************************************
+ CONFIGURATION
+***************************************************************************************************/
+SET @CHARACTER = 1;
+SET @VALUE = 400;
+SET @MAX = 400;
+-- *************************************************************************************************
 
 DELETE
 FROM `character_skills`
@@ -17,11 +25,11 @@ WHERE `guid` = @CHARACTER
   AND `skill` IN (44, 54, 95, 136, 160, 162, 172, 173);
 
 INSERT INTO `character_skills` (`guid`, `skill`, `value`, `max`)
-VALUES (@CHARACTER, 44, 400, 400),
-       (@CHARACTER, 54, 400, 400),
-       (@CHARACTER, 95, 400, 400),
-       (@CHARACTER, 136, 400, 400),
-       (@CHARACTER, 160, 400, 400),
-       (@CHARACTER, 162, 400, 400),
-       (@CHARACTER, 172, 400, 400),
-       (@CHARACTER, 173, 400, 400);
+VALUES (@CHARACTER, 44, @VALUE, @MAX),
+       (@CHARACTER, 54, @VALUE, @MAX),
+       (@CHARACTER, 95, @VALUE, @MAX),
+       (@CHARACTER, 136, @VALUE, @MAX),
+       (@CHARACTER, 160, @VALUE, @MAX),
+       (@CHARACTER, 162, @VALUE, @MAX),
+       (@CHARACTER, 172, @VALUE, @MAX),
+       (@CHARACTER, 173, @VALUE, @MAX);
