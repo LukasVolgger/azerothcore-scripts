929f66f592e8a7064666fab242824e8667f0f1c5
Lukas Volgger <lukas.volgger@live.at>
2023-08-05 21:03:01 +0200

    feat/deathknight: add class mounts script

---
+++ sql/characters/classes/deathknight/3_get_all_class_mounts.sql
@@ -0,0 +1,10 @@
+SET
+@CHARACTER = 1;
+
+-- Acherus Deathcharger
+REPLACE
+INTO `character_spell` (`guid`, `spell`, `specMask`) VALUES (@CHARACTER, 48778, 255);
+
+-- Winged Steed of the Ebon Blade
+REPLACE
+INTO `character_spell` (`guid`, `spell`, `specMask`) VALUES (@CHARACTER, 54729, 255);
