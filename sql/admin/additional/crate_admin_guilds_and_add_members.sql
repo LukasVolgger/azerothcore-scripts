ef44398c6df77897eca2a35653bca32a321a0bcd
Lukas Volgger <lukas.volgger@live.at>
2023-08-05 23:04:45 +0200

    feat/admin: add docs and config to admin guilds script

--- sql/admin/additional/crate_admin_guilds_and_add_members.sql
+++ sql/admin/additional/crate_admin_guilds_and_add_members.sql
@@ -1,18 +1,47 @@
+/***************************************************************************************************
+Faction:            Alliance / Horde / Both
+DB:                 characters
+DB Version:         2023-08-05
+Author:             Lukas Volgger
+****************************************************************************************************
+DESCRIPTION
+----------------------------------------------------------------------------------------------------
+- Creates all admin guilds with max money (214k)
+- Adds all admin characters to guilds
+- Unlocks all guild bank tabs
+- Create a standard guild emblem
+- All event logs were also added
+***************************************************************************************************/
+
+/***************************************************************************************************
+ CONFIGURATION
+***************************************************************************************************/
+SET @ADMINGUILDNAME = 'Administrators';
+SET @ALLISNCEADMINGUILDNAME = 'Alliance Administrators';
+SET @HORDEADMINGUILDNAME = 'Horde Administrators';
+SET @CREATEDATE = 1691263014;
+SET @BANKMONEY = 2140000000;
+SET @GUILDRANK0 = 'Gildenmeister';
+SET @GUILDRANK1 = 'Offizier';
+SET @GUILDRANK2 = 'Veteran';
+SET @GUILDRANK3 = 'Mitglied';
+SET @GUILDRANK4 = 'Anfänger';
+
 -- guild
 DELETE FROM `guild` WHERE `guildid` IN (1, 2, 3);
-INSERT INTO `guild` (`guildid`, `name`, `leaderguid`, `EmblemStyle`, `EmblemColor`, `BorderStyle`, `BorderColor`, `BackgroundColor`, `info`, `motd`, `createdate`, `BankMoney`) VALUES (1, 'Administrators', 1, 20, 16, 0, 16, 35, '', 'No message set.', 1691263014, 2140000000);
-INSERT INTO `guild` (`guildid`, `name`, `leaderguid`, `EmblemStyle`, `EmblemColor`, `BorderStyle`, `BorderColor`, `BackgroundColor`, `info`, `motd`, `createdate`, `BankMoney`) VALUES (2, 'Alliance Administrators', 2, 20, 10, 0, 10, 32, '', 'No message set.', 1691263014, 2140000000);
-INSERT INTO `guild` (`guildid`, `name`, `leaderguid`, `EmblemStyle`, `EmblemColor`, `BorderStyle`, `BorderColor`, `BackgroundColor`, `info`, `motd`, `createdate`, `BankMoney`) VALUES (3, 'Horde Administrators', 12, 20, 1, 0, 0, 44, '', 'No message set.', 1691263014, 2140000000);
+INSERT INTO `guild` (`guildid`, `name`, `leaderguid`, `EmblemStyle`, `EmblemColor`, `BorderStyle`, `BorderColor`, `BackgroundColor`, `info`, `motd`, `createdate`, `BankMoney`) VALUES (1, @ADMINGUILDNAME, 1, 20, 16, 0, 16, 35, '', 'No message set.', @CREATEDATE, @BANKMONEY);
+INSERT INTO `guild` (`guildid`, `name`, `leaderguid`, `EmblemStyle`, `EmblemColor`, `BorderStyle`, `BorderColor`, `BackgroundColor`, `info`, `motd`, `createdate`, `BankMoney`) VALUES (2, @ALLISNCEADMINGUILDNAME, 2, 20, 10, 0, 10, 32, '', 'No message set.', @CREATEDATE, @BANKMONEY);
+INSERT INTO `guild` (`guildid`, `name`, `leaderguid`, `EmblemStyle`, `EmblemColor`, `BorderStyle`, `BorderColor`, `BackgroundColor`, `info`, `motd`, `createdate`, `BankMoney`) VALUES (3, @HORDEADMINGUILDNAME, 12, 20, 1, 0, 0, 44, '', 'No message set.', @CREATEDATE, @BANKMONEY);
 
 -- guild_bank_eventlog
 DELETE FROM `guild_bank_eventlog` WHERE `guildid`=1 AND `LogGuid`=0 AND `TabId`=100;
-INSERT INTO `guild_bank_eventlog` (`guildid`, `LogGuid`, `TabId`, `EventType`, `PlayerGuid`, `ItemOrMoney`, `ItemStackCount`, `DestTabId`, `TimeStamp`) VALUES (1, 0, 100, 4, 1, 2140000000, 0, 0, 1691263014);
+INSERT INTO `guild_bank_eventlog` (`guildid`, `LogGuid`, `TabId`, `EventType`, `PlayerGuid`, `ItemOrMoney`, `ItemStackCount`, `DestTabId`, `TimeStamp`) VALUES (1, 0, 100, 4, 1, 2140000000, 0, 0, @CREATEDATE);
 -- Alliance
 DELETE FROM `guild_bank_eventlog` WHERE `guildid`=2 AND `LogGuid`=0 AND `TabId`=100;
-INSERT INTO `guild_bank_eventlog` (`guildid`, `LogGuid`, `TabId`, `EventType`, `PlayerGuid`, `ItemOrMoney`, `ItemStackCount`, `DestTabId`, `TimeStamp`) VALUES (2, 0, 100, 4, 2, 2140000000, 0, 0, 1691263014);
+INSERT INTO `guild_bank_eventlog` (`guildid`, `LogGuid`, `TabId`, `EventType`, `PlayerGuid`, `ItemOrMoney`, `ItemStackCount`, `DestTabId`, `TimeStamp`) VALUES (2, 0, 100, 4, 2, 2140000000, 0, 0, @CREATEDATE);
 -- Horde
 DELETE FROM `guild_bank_eventlog` WHERE `guildid`=3 AND `LogGuid`=0 AND `TabId`=100;
-INSERT INTO `guild_bank_eventlog` (`guildid`, `LogGuid`, `TabId`, `EventType`, `PlayerGuid`, `ItemOrMoney`, `ItemStackCount`, `DestTabId`, `TimeStamp`) VALUES (3, 0, 100, 4, 12, 2140000000, 0, 0, 1691263014);
+INSERT INTO `guild_bank_eventlog` (`guildid`, `LogGuid`, `TabId`, `EventType`, `PlayerGuid`, `ItemOrMoney`, `ItemStackCount`, `DestTabId`, `TimeStamp`) VALUES (3, 0, 100, 4, 12, 2140000000, 0, 0, @CREATEDATE);
 
 -- guild_bank_right
 DELETE FROM `guild_bank_right` WHERE `guildid`=1 AND `TabId`=0 AND `rid`=0;
@@ -240,49 +269,49 @@ INSERT INTO `guild_bank_tab` (`guildid`, `TabId`, `TabName`, `TabIcon`, `TabText
 
 -- guild_eventlog
 DELETE FROM `guild_eventlog` WHERE `guildid`=1 AND `LogGuid`=0;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (1, 0, 2, 1, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (1, 0, 2, 1, 0, 0, @CREATEDATE);
 -- Alliance
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=0;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 0, 2, 2, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 0, 2, 2, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=1;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 1, 2, 3, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 1, 2, 3, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=2;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 2, 2, 4, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 2, 2, 4, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=3;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 3, 2, 5, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 3, 2, 5, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=4;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 4, 2, 6, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 4, 2, 6, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=5;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 5, 2, 7, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 5, 2, 7, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=6;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 6, 2, 8, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 6, 2, 8, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=7;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 7, 2, 9, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 7, 2, 9, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=8;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 8, 2, 10, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 8, 2, 10, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=2 AND `LogGuid`=9;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 9, 2, 11, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (2, 9, 2, 11, 0, 0, @CREATEDATE);
 -- Horde
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=0;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 0, 2, 12, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 0, 2, 12, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=1;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 1, 2, 13, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 1, 2, 13, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=2;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 2, 2, 14, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 2, 2, 14, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=3;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 3, 2, 15, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 3, 2, 15, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=4;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 4, 2, 16, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 4, 2, 16, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=5;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 5, 2, 17, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 5, 2, 17, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=6;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 6, 2, 18, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 6, 2, 18, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=7;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 7, 2, 19, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 7, 2, 19, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=8;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 8, 2, 20, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 8, 2, 20, 0, 0, @CREATEDATE);
 DELETE FROM `guild_eventlog` WHERE `guildid`=3 AND `LogGuid`=9;
-INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 9, 2, 21, 0, 0, 1691263014);
+INSERT INTO `guild_eventlog` (`guildid`, `LogGuid`, `EventType`, `PlayerGuid1`, `PlayerGuid2`, `NewRank`, `TimeStamp`) VALUES (3, 9, 2, 21, 0, 0, @CREATEDATE);
 
 -- guild_member - Guild
 DELETE FROM `guild_member` WHERE `guid` IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21);
@@ -313,32 +342,32 @@ INSERT INTO `guild_member` (`guildid`, `guid`, `rank`, `pnote`, `offnote`) VALUE
 
 -- guild_rank
 DELETE FROM `guild_rank` WHERE `guildid`=1 AND `rid`=0;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 0, 'Gildenmeister', 1962495, 4294967295);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 0, @GUILDRANK0, 1962495, 4294967295);
 DELETE FROM `guild_rank` WHERE `guildid`=1 AND `rid`=1;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 1, 'Offizier', 1962495, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 1, @GUILDRANK1, 1962495, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=1 AND `rid`=2;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 2, 'Veteran', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 2, @GUILDRANK2, 67, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=1 AND `rid`=3;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 3, 'Mitglied', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 3, @GUILDRANK3, 67, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=1 AND `rid`=4;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 4, 'Anfänger', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (1, 4, @GUILDRANK4, 67, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=2 AND `rid`=0;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 0, 'Gildenmeister', 1962495, 4294967295);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 0, @GUILDRANK0, 1962495, 4294967295);
 DELETE FROM `guild_rank` WHERE `guildid`=2 AND `rid`=1;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 1, 'Offizier', 1962495, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 1, @GUILDRANK1, 1962495, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=2 AND `rid`=2;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 2, 'Veteran', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 2, @GUILDRANK2, 67, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=2 AND `rid`=3;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 3, 'Mitglied', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 3, @GUILDRANK3, 67, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=2 AND `rid`=4;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 4, 'Anfänger', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (2, 4, @GUILDRANK4, 67, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=3 AND `rid`=0;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 0, 'Gildenmeister', 1962495, 4294967295);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 0, @GUILDRANK0, 1962495, 4294967295);
 DELETE FROM `guild_rank` WHERE `guildid`=3 AND `rid`=1;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 1, 'Offizier', 1962495, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 1, @GUILDRANK1, 1962495, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=3 AND `rid`=2;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 2, 'Veteran', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 2, @GUILDRANK2, 67, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=3 AND `rid`=3;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 3, 'Mitglied', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 3, @GUILDRANK3, 67, 0);
 DELETE FROM `guild_rank` WHERE `guildid`=3 AND `rid`=4;
-INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 4, 'Anfänger', 67, 0);
+INSERT INTO `guild_rank` (`guildid`, `rid`, `rname`, `rights`, `BankMoneyPerDay`) VALUES (3, 4, @GUILDRANK4, 67, 0);
