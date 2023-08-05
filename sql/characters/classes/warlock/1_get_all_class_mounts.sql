9f0749c5d1c63bded190ed89903711824271de1b
Lukas Volgger <lukas.volgger@live.at>
2023-08-05 20:52:24 +0200

    feat/warlock: add class mounts script

---
+++ sql/characters/classes/warlock/1_get_all_class_mounts.sql
@@ -0,0 +1,10 @@
+SET
+@CHARACTER = 1;
+
+-- Felsteed
+REPLACE
+INTO `character_spell` (`guid`, `spell`, `specMask`) VALUES (@CHARACTER, 5784, 255);
+
+-- Dreadsteed
+REPLACE
+INTO `character_spell` (`guid`, `spell`, `specMask`) VALUES (@CHARACTER, 23161, 255);
