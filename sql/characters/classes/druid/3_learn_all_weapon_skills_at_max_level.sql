eed11636185ca035427cb7a6bef598041462ae1c
Lukas Volgger <lukas.volgger@live.at>
2023-08-05 23:57:47 +0200

    feat/druid: add docs and config to scripts

--- sql/characters/classes/druid/3_learn_all_weapon_skills_at_max_level.sql
+++ sql/characters/classes/druid/3_learn_all_weapon_skills_at_max_level.sql
@@ -1,14 +1,23 @@
--- Sets all possible weapon skills (also the learnable ones) to the highest level
--- 54 = Maces
--- 95 = Defense
--- 136 = Staves
--- 160 = Two-Handed Maces
--- 162 = Unarmed
--- 173 = Daggers
--- 229 = Polearms
+/***************************************************************************************************
+Faction:            Both
+DB:                 characters
+DB Version:         2023-08-05
+Author:             Lukas Volgger
+****************************************************************************************************
+DESCRIPTION
+----------------------------------------------------------------------------------------------------
+- Sets all possible weapon skills (also the learnable ones) to the highest level
+- 54 = Maces, 95 = Defense, 136 = Staves, 160 = Two-Handed Maces, 162 = Unarmed,
+  173 = Daggers, 229 = Polearms
+***************************************************************************************************/
 
-SET
-@CHARACTER = 1;
+/***************************************************************************************************
+ CONFIGURATION
+***************************************************************************************************/
+SET @CHARACTER = 1;
+SET @VALUE = 400;
+SET @MAX = 400;
+-- *************************************************************************************************
 
 DELETE
 FROM `character_skills`
