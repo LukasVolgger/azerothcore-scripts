f19d9b10ccf904c3fe5e4acfe154b1ecc3f7b2c4
Lukas Volgger <lukas.volgger@live.at>
2023-08-06 00:03:09 +0200

    feat/hunter: add docs and config to scripts

--- sql/characters/classes/hunter/3_learn_all_weapon_skills_at_max_level.sql
+++ sql/characters/classes/hunter/3_learn_all_weapon_skills_at_max_level.sql
@@ -1,20 +1,24 @@
--- Sets all possible weapon skills (also the learnable ones) to the highest level
--- 43 = Swords
--- 44 = Axes
--- 45 = Bows
--- 46 = Guns
--- 55 = Two-Handed Swords
--- 95 = Defense
--- 136 = Staves
--- 162 = Unarmed
--- 172 = Two-Handed Axes
--- 173 = Daggers
--- 176 = Thrown
--- 226 = Crossbows
--- 229 = Polearms
+/***************************************************************************************************
+Faction:            Both
+DB:                 characters
+DB Version:         2023-08-05
+Author:             Lukas Volgger
+****************************************************************************************************
+DESCRIPTION
+----------------------------------------------------------------------------------------------------
+- Sets all possible weapon skills (also the learnable ones) to the highest level
+- 43 = Swords, 44 = Axes, 45 = Bows, 46 = Guns, 55 = Two-Handed Swords, 95 = Defense,
+  136 = Staves, 162 = Unarmed, 172 = Two-Handed Axes, 173 = Daggers, 176 = Thrown,
+  226 = Crossbows, 229 = Polearms
+***************************************************************************************************/
 
-SET
-@CHARACTER = 1;
+/***************************************************************************************************
+ CONFIGURATION
+***************************************************************************************************/
+SET @CHARACTER = 1;
+SET @VALUE = 400;
+SET @MAX = 400;
+-- *************************************************************************************************
 
 DELETE
 FROM `character_skills`
@@ -22,16 +26,16 @@ WHERE `guid` = @CHARACTER
   AND `skill` IN (43, 44, 45, 46, 55, 95, 136, 162, 172, 173, 176, 226, 229);
 
 INSERT INTO `character_skills` (`guid`, `skill`, `value`, `max`)
-VALUES (@CHARACTER, 43, 400, 400),
-       (@CHARACTER, 44, 400, 400),
-       (@CHARACTER, 45, 400, 400),
-       (@CHARACTER, 46, 400, 400),
-       (@CHARACTER, 55, 400, 400),
-       (@CHARACTER, 95, 400, 400),
-       (@CHARACTER, 136, 400, 400),
-       (@CHARACTER, 162, 400, 400),
-       (@CHARACTER, 172, 400, 400),
-       (@CHARACTER, 173, 400, 400),
-       (@CHARACTER, 176, 400, 400),
-       (@CHARACTER, 226, 400, 400),
-       (@CHARACTER, 229, 400, 400);
+VALUES (@CHARACTER, 43, @VALUE, @MAX),
+       (@CHARACTER, 44, @VALUE, @MAX),
+       (@CHARACTER, 45, @VALUE, @MAX),
+       (@CHARACTER, 46, @VALUE, @MAX),
+       (@CHARACTER, 55, @VALUE, @MAX),
+       (@CHARACTER, 95, @VALUE, @MAX),
+       (@CHARACTER, 136, @VALUE, @MAX),
+       (@CHARACTER, 162, @VALUE, @MAX),
+       (@CHARACTER, 172, @VALUE, @MAX),
+       (@CHARACTER, 173, @VALUE, @MAX),
+       (@CHARACTER, 176, @VALUE, @MAX),
+       (@CHARACTER, 226, @VALUE, @MAX),
+       (@CHARACTER, 229, @VALUE, @MAX);
