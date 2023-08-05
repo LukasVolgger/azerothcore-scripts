285f20c9c0882f6e19cebbfe98e839e4bd23fa37
Lukas Volgger <lukas.volgger@live.at>
2023-08-06 00:21:55 +0200

    feat/warrior: add docs and config to scripts

--- sql/characters/classes/warrior/3_learn_all_weapon_skills_at_max_level.sql
+++ sql/characters/classes/warrior/3_learn_all_weapon_skills_at_max_level.sql
@@ -1,22 +1,24 @@
--- Sets all possible weapon skills (also the learnable ones) to the highest level
--- 43 = Swords
--- 44 = Axes
--- 45 = Bows
--- 46 = Guns
--- 54 = Maces
--- 55 = Two-Handed Swords
--- 95 = Defense
--- 136 = Staves
--- 160 = Two-Handed Maces
--- 162 = Unarmed
--- 172 = Two-Handed Axes
--- 173 = Daggers
--- 176 = Thrown
--- 226 = Crossbows
--- 229 = Polearms
+/***************************************************************************************************
+Faction:            Both
+DB:                 characters
+DB Version:         2023-08-05
+Author:             Lukas Volgger
+****************************************************************************************************
+DESCRIPTION
+----------------------------------------------------------------------------------------------------
+- Sets all possible weapon skills (also the learnable ones) to the highest level
+- 43 = Swords, 44 = Axes, 45 = Bows, 46 = Guns, 54 = Maces, 55 = Two-Handed Swords, 95 = Defense,
+  136 = Staves, 160 = Two-Handed Maces, 162 = Unarmed, 172 = Two-Handed Axes, 173 = Daggers,
+  176 = Thrown, 226 = Crossbows, 229 = Polearms
+***************************************************************************************************/
 
-SET
-@CHARACTER = 1;
+/***************************************************************************************************
+ CONFIGURATION
+***************************************************************************************************/
+SET @CHARACTER = 1;
+SET @VALUE = 400;
+SET @MAX = 400;
+-- *************************************************************************************************
 
 DELETE
 FROM `character_skills`
@@ -24,18 +26,18 @@ WHERE `guid` = @CHARACTER
   AND `skill` IN (43, 44, 45, 46, 54, 55, 95, 136, 160, 162, 172, 173, 176, 226, 229);
 
 INSERT INTO `character_skills` (`guid`, `skill`, `value`, `max`)
-VALUES (@CHARACTER, 43, 400, 400),
-       (@CHARACTER, 44, 400, 400),
-       (@CHARACTER, 45, 400, 400),
-       (@CHARACTER, 46, 400, 400),
-       (@CHARACTER, 54, 400, 400),
-       (@CHARACTER, 55, 400, 400),
-       (@CHARACTER, 95, 400, 400),
-       (@CHARACTER, 136, 400, 400),
-       (@CHARACTER, 160, 400, 400),
-       (@CHARACTER, 162, 400, 400),
-       (@CHARACTER, 172, 400, 400),
-       (@CHARACTER, 173, 400, 400),
-       (@CHARACTER, 176, 400, 400),
-       (@CHARACTER, 226, 400, 400),
-       (@CHARACTER, 229, 400, 400);
+VALUES (@CHARACTER, 43, @VALUE, @MAX),
+       (@CHARACTER, 44, @VALUE, @MAX),
+       (@CHARACTER, 45, @VALUE, @MAX),
+       (@CHARACTER, 46, @VALUE, @MAX),
+       (@CHARACTER, 54, @VALUE, @MAX),
+       (@CHARACTER, 55, @VALUE, @MAX),
+       (@CHARACTER, 95, @VALUE, @MAX),
+       (@CHARACTER, 136, @VALUE, @MAX),
+       (@CHARACTER, 160, @VALUE, @MAX),
+       (@CHARACTER, 162, @VALUE, @MAX),
+       (@CHARACTER, 172, @VALUE, @MAX),
+       (@CHARACTER, 173, @VALUE, @MAX),
+       (@CHARACTER, 176, @VALUE, @MAX),
+       (@CHARACTER, 226, @VALUE, @MAX),
+       (@CHARACTER, 229, @VALUE, @MAX);
